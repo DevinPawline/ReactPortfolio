fbdad4c336408507384f66fdfbbefaa83ff3279d
Devin Pawline <devinpawline@outlook.com>
2021-09-18 10:20:09 -0400

    fist commit

---
+++ reactportfolio/src/resumeData.js
@@ -0,0 +1,100 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Rahul Bhatia",
+    "role": "Frontend Developer and Data Scientist",
+    "linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/devin-pawline-b3750a215/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/DevinPawline",
+          "className":"fa fa-github"
+        }
+      ],
+    "aboutme":"I am currently a full-stack student at UNCC Coding Bootcamp! I have a passion for technology and coding and look forward to creating a project for you! ",
+    "address":"India",
+    "website":"https://rbhatia46.github.io",
+    "education":[
+      {
+        "UniversityName":"University of North Carolina at Charlotte",
+        "specialization":"Full Stack Coding",
+        "MonthOfPassing":"October",
+        "YearOfPassing":"2021",
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Brace Family YMCA",
+        "specialization":"Summer Camp Assistant Director",
+        "MonthOfLeaving":"Aug",
+        "YearOfLeaving":"2019",
+      },
+      {
+        "CompanyName":"Brace Family YMCA",
+        "specialization":"Teens Program Co-Director",
+        "MonthOfLeaving":"Aug",
+        "YearOfLeaving":"2019",
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"Vanilla Java"
+      },
+      {
+        "skillname":"Mongo"
+      },
+      {
+        "skillname":"Microsoft Certified "
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
